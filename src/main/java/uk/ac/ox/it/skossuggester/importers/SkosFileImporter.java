349797b0589062c7941596f943e1e54faa61be59
Martin Filliau <martin@filliau.com>
2014-06-12 14:23:11 +0100

    Added JENA as a dependency

--- src/main/java/uk/ac/ox/it/skossuggester/importers/SkosFileImporter.java
+++ src/main/java/uk/ac/ox/it/skossuggester/importers/SkosFileImporter.java
@@ -1,13 +1,21 @@
 package uk.ac.ox.it.skossuggester.importers;
 
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.util.FileManager;
 import io.dropwizard.cli.ConfiguredCommand;
 import io.dropwizard.setup.Bootstrap;
+import java.io.InputStream;
 import net.sourceforge.argparse4j.inf.Namespace;
+import org.apache.solr.common.SolrDocument;
 import uk.ac.ox.it.skossuggester.configuration.AppConfiguration;
 
 
 public class SkosFileImporter extends ConfiguredCommand<AppConfiguration> {
-
+    
+    private Model model;
+    
     public SkosFileImporter() {
         super("skosimport", "Import SKOS file");
     }
@@ -17,4 +25,21 @@ public class SkosFileImporter extends ConfiguredCommand<AppConfiguration> {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
+    private void importFile(String location) {
+        model = ModelFactory.createDefaultModel();
+        InputStream in = FileManager.get().open(location);
+        if (in == null) {
+            throw new IllegalArgumentException("File: " + location + " not found");
+        }
+        model.read(in, null);
+        
+        // iterate over Resources having rdf:type schema.org/Topic
+        
+    }
+    
+    private SolrDocument getDocument(Resource res) {
+        SolrDocument doc = new SolrDocument();
+        
+        return doc;
+    }
 }
