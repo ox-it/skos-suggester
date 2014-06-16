dfeb55710ad5c638ef0f6222fd8b34aa1a437230
Martin Filliau <martin@filliau.com>
2014-06-16 14:46:28 +0200

    Initial GET handler

--- src/main/java/uk/ac/ox/it/skossuggester/SkosSuggesterApplication.java
+++ src/main/java/uk/ac/ox/it/skossuggester/SkosSuggesterApplication.java
@@ -3,6 +3,7 @@ package uk.ac.ox.it.skossuggester;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.apache.solr.client.solrj.impl.HttpSolrServer;
 import org.eclipse.jetty.servlets.CrossOriginFilter;
 import uk.ac.ox.it.skossuggester.configuration.AppConfiguration;
 import uk.ac.ox.it.skossuggester.health.SolrHealth;
@@ -21,13 +22,14 @@ public class SkosSuggesterApplication extends Application<AppConfiguration>{
     @Override
     public void run(AppConfiguration configuration, Environment environment) throws Exception {
         environment.servlets().addFilter("/*", CrossOriginFilter.class);
+        final HttpSolrServer solr = new HttpSolrServer(configuration.getSolrLocation());
         final Suggest suggest = new Suggest();
         final Search search = new Search();
-        final Get get = new Get();
+        final Get get = new Get(solr);
         environment.jersey().register(suggest);
         environment.jersey().register(search);
         environment.jersey().register(get);
-        final SolrHealth solrHealth = new SolrHealth();
+        final SolrHealth solrHealth = new SolrHealth(solr);
         environment.healthChecks().register("solr", solrHealth);
     }
     
--- src/main/java/uk/ac/ox/it/skossuggester/resources/Get.java
+++ src/main/java/uk/ac/ox/it/skossuggester/resources/Get.java
@@ -1,11 +1,52 @@
 package uk.ac.ox.it.skossuggester.resources;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.BinaryResponseParser;
+import org.apache.solr.client.solrj.impl.HttpSolrServer;
+import org.apache.solr.client.solrj.request.QueryRequest;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
 
 @Path("/get")
 @Produces(MediaType.APPLICATION_JSON)
 public class Get {
 
+    private final HttpSolrServer solr;
+    
+    public Get(HttpSolrServer solr) {
+        this.solr = solr;
+    }
+    
+    @GET
+    public String get(@QueryParam("uri") String uri) {
+        SolrQuery q = new SolrQuery();
+        q.setRequestHandler("/get");
+        q.set("id", uri);
+        QueryRequest req = new QueryRequest(q);
+        req.setResponseParser(new BinaryResponseParser());
+        QueryResponse rsp;
+        try {
+            rsp = req.process(solr);
+            SolrDocument out = (SolrDocument)rsp.getResponse().get("doc");
+            if (out != null) {
+                return out.toString();
+                //return Tag.fromSolrDocument(out);
+            } else {
+                throw new WebApplicationException(404);
+            }
+        } catch (SolrServerException ex) {
+            Logger.getLogger(Get.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+
+    }
 }
