ff5cf87e74905f77d293a30375ea071c5d677dca
Martin Filliau <martin@filliau.com>
2014-06-16 14:38:12 +0200

    Added Solr Healthcheck

--- src/main/java/uk/ac/ox/it/skossuggester/health/SolrHealth.java
+++ src/main/java/uk/ac/ox/it/skossuggester/health/SolrHealth.java
@@ -1,13 +1,26 @@
 package uk.ac.ox.it.skossuggester.health;
 
 import com.codahale.metrics.health.HealthCheck;
+import org.apache.solr.client.solrj.impl.HttpSolrServer;
+import org.apache.solr.client.solrj.response.SolrPingResponse;
 
 
 public class SolrHealth extends HealthCheck{
 
-    @Override
-    protected Result check() throws Exception {
-        return Result.unhealthy("TODO");
+    private final HttpSolrServer solr;
+    
+    public SolrHealth(HttpSolrServer solr) {
+        this.solr = solr;
     }
     
+    @Override
+    protected Result check() throws Exception {
+        SolrPingResponse response = this.solr.ping();
+        // 0 means OK
+        if (response.getStatus() == 0) {
+            return Result.healthy();
+        } else {
+            return Result.unhealthy(response.toString());
+        }   
+    }
 }
