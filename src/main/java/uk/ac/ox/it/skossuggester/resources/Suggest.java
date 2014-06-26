4a6ef645cb86674d2ca6156e84a2fd105b52f9df
Martin Filliau <martin@filliau.com>
2014-06-26 16:55:43 +0100

    HAL suggest

--- src/main/java/uk/ac/ox/it/skossuggester/resources/Suggest.java
+++ src/main/java/uk/ac/ox/it/skossuggester/resources/Suggest.java
@@ -7,9 +7,12 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriBuilder;
 import uk.ac.ox.it.skossuggester.dao.SkosConceptsDao;
 import uk.ac.ox.it.skossuggester.jerseyutils.PositiveIntParam;
 import uk.ac.ox.it.skossuggester.representations.SkosConcepts;
+import uk.ac.ox.it.skossuggester.representations.hal.HalLink;
+import uk.ac.ox.it.skossuggester.representations.hal.HalRepresentation;
 
 @Path("/suggest")
 @Produces(MediaType.APPLICATION_JSON)
@@ -21,12 +24,14 @@ public class Suggest {
     }
     
     @GET
-    public SkosConcepts suggest(@QueryParam("q") String query,
+    public HalRepresentation suggest(@QueryParam("q") String query,
                                 @QueryParam("page") @DefaultValue("1") PositiveIntParam page,
                                 @QueryParam("count") @DefaultValue("20") PositiveIntParam count) {
         int firstResult = PaginationUtils.getFirstResult(page.get(), count.get());
         Optional<SkosConcepts> concepts = dao.suggest(query, firstResult, count.get());
-        return concepts.or(new SkosConcepts());
+        HalRepresentation hal = new HalRepresentation();
+        hal.setSelfLink(new HalLink(UriBuilder.fromResource(Suggest.class).queryParam("q", query).build().toString()));
+        hal.setEmbedded(concepts.or(new SkosConcepts()));
+        return hal;
     }
-
 }
