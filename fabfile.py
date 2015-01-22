d2eee38a8d61f0c6e1dce03b326a8f7af8261bd3
Martin Filliau <martin@filliau.com>
2015-01-22 16:27:28 +0000

    Updated fabric to handle local os x use case as well

--- fabfile.py
+++ fabfile.py
@@ -2,6 +2,7 @@ import os
 from datetime import datetime
 
 from fabric.api import *
+from fabric.operations import run, local
 from fabric.contrib import *
 from fabric.contrib.files import exists
 from fabric import utils
@@ -13,10 +14,22 @@ FAST_TOPICS_URL = "http://experimental.worldcat.org/fast/download/FASTTopical.nt
 Environments
 """
 
-ENVIRONMENTS = ('dev', 'staging')
+@task
+def local_osx():
+    """Local OS X, assuming tdbloader2 is available
+    """
+    env.skos_home = '~/talks'
+    env.current_skos = '~/talks/current_fast_topics.nt'
+    env.tdb_store = '~/talks/tdb_store'
+    env.skos_jar = '~/Documents/Projects/skos-suggester/target/skos-suggester-1.0-SNAPSHOT.jar'
+    env.skos_jar_conf_file = '~/Documents/Projects/skos-suggester/src/main/resources/configuration.yml'
+    env.tdbloader2_path = 'tdbloader2'
+    env.run = local
 
 @task
-def local():
+def vm():
+    """Local VM
+    """
     env.hosts = ['talks.vm']
     env.user = 'talks'
     env.skos_home = '/srv/talks'
@@ -25,10 +38,12 @@ def local():
     env.skos_jar = '/srv/talks/skos-suggester-1.0-SNAPSHOT.jar'
     env.skos_jar_conf_file = '/srv/talks/skos-suggester-configuration.yaml'
     env.tdbloader2_path = '/srv/jena/apache-jena-2.12.1/bin/tdbloader2'
-
+    env.run = run
 
 @task
 def staging():
+    """Talks staging environment
+    """
     env.hosts = ['talks-dev.oucs.ox.ac.uk']
     env.user = 'talks'
     env.skos_home = '/srv/talks'
@@ -37,6 +52,7 @@ def staging():
     env.skos_jar = '/srv/talks/skos-suggester-1.0-SNAPSHOT.jar'
     env.skos_jar_conf_file = '/srv/talks/skos-suggester-configuration.yaml'
     env.tdbloader2_path = '/srv/jena/apache-jena-2.12.1/bin/tdbloader2'
+    env.run = run
 
 
 """
@@ -49,24 +65,24 @@ def download_fast_topics():
     """
     fast_file = '%s-fast-topics.nt' % (datetime.now().strftime('%Y%m%d%H%M'))
     extracted_file = 'FASTTopical.nt'
-    run('curl -L --progress-bar "{fast_download}" > {home}/{file}.zip'.format(fast_download=FAST_TOPICS_URL,
+    env.run('curl -L --progress-bar "{fast_download}" > {home}/{file}.zip'.format(fast_download=FAST_TOPICS_URL,
                                                                                 home=env.skos_home,
                                                                                 file=fast_file))
-    run('unzip {home}/{file}.zip {extracted} -d {home}'.format(home=env.skos_home,
+    env.run('unzip {home}/{file}.zip {extracted} -d {home}'.format(home=env.skos_home,
                                                                 file=fast_file,
                                                                 extracted=extracted_file))
-    run('rm -f {home}/{file}.zip'.format(home=env.skos_home, file=fast_file))
-    run('mv {home}/{extracted} {home}/{file}'.format(home=env.skos_home, file=fast_file, extracted=extracted_file))
-    run('rm -f {path}'.format(path=env.current_skos))
-    run('ln -s {home}/{file} {current}'.format(home=env.skos_home, file=fast_file, current=env.current_skos))
+    env.run('rm -f {home}/{file}.zip'.format(home=env.skos_home, file=fast_file))
+    env.run('mv {home}/{extracted} {home}/{file}'.format(home=env.skos_home, file=fast_file, extracted=extracted_file))
+    env.run('rm -f {path}'.format(path=env.current_skos))
+    env.run('ln -s {home}/{file} {current}'.format(home=env.skos_home, file=fast_file, current=env.current_skos))
 
 @task
 def load_into_tdb():
     """Load current SKOS file into TDB
     """
     # first, make sure it is a new store
-    run('rm -rf {tdb_store}'.format(tdb_store=env.tdb_store))
-    run('{tdbloader2} -loc {tdb_store} {skos_file}'.format(tdbloader2=env.tdbloader2_path,
+    env.run('rm -rf {tdb_store}'.format(tdb_store=env.tdb_store))
+    env.run('{tdbloader2} -loc {tdb_store} {skos_file}'.format(tdbloader2=env.tdbloader2_path,
                                                             tdb_store=env.tdb_store,
                                                             skos_file=env.current_skos))
 
@@ -74,7 +90,7 @@ def load_into_tdb():
 def run_importer():
     """Run the importer into Apache Solr
     """
-    run('java -jar {skos_jar} tdbimport -d {tdb_store} {skos_jar_conf}'.format(skos_jar=env.skos_jar,
+    env.run('java -jar {skos_jar} tdbimport -d {tdb_store} {skos_jar_conf}'.format(skos_jar=env.skos_jar,
                                                                                 tdb_store=env.tdb_store,
                                                                                 skos_jar_conf=env.skos_jar_conf_file))
 
